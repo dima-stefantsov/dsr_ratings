c11a02d8f7ce0390bedae8d02f34eca2d3faf860
Dima Stefantsov <dima@stefantsov.com>
2023-01-23 22:15:00 +0300

    main_next double mirror rating change

--- calculators/main_next.php
+++ calculators/main_next.php
@@ -11,11 +11,12 @@ $GLOBALS['rating_calculators']['main_next'] = [
         - uncompressing before comparisons
         - fix 2k-border gravity bug
         - improved rewards calculations
+        - mirror matchup gives x2 rating changes
 
         please give feedback on it in <a target="_blank" href="https://discord.gg/KXKw8HqKKK">discord</a>
         ',
     'default_rating' => 2000,
-    'fields' => [],
+    'fields' => ['gp.race'],
     'main' => 'dsr_main_next__main',
     ];
 
@@ -53,6 +54,8 @@ function dsr_main_next__compress_rating(&$teams) {
 }
 
 function dsr_main_next__update_ratings(&$teams) {
+    $full_mirror_multiplier = dsr_main_next__get_full_mirror_multiplier($teams);
+
     $factor = 4000;
 
     $team_ratings = [];
@@ -66,24 +69,53 @@ function dsr_main_next__update_ratings(&$teams) {
 
     foreach ($teams as &$team) {
         foreach ($team as &$player) {
-            $multiplier = dsr_main_next__get_multiplier($player['status']);
+            $status_multiplier = dsr_main_next__get_status_multiplier($player['status']);
 
             $reward = $rewards[$player['winner_team']];
             if ($player['team'] === $player['winner_team'] && $player['status'] !== 'win') {
                 $reward = $rewards[$player['winner_team']+1];
             }
 
-            $diff = intval($multiplier*$reward);
+            $diff = $reward*$full_mirror_multiplier*$status_multiplier;
             if ($player['rating'] + $diff < 20000) {
-                $diff = intval($multiplier*max(10, min(90, $reward)));
+                $diff = max(1, floor($reward*$full_mirror_multiplier/10))*10 * $status_multiplier;
             }
 
-            $player['rating'] = max(1, $player['rating'] + $diff);
+            $player['rating'] = max(1, $player['rating'] + intval($diff));
         }
     }
 }
 
-function dsr_main_next__get_multiplier($status) {
+function dsr_main_next__get_full_mirror_multiplier(&$teams) {
+    $races0 = [];
+    foreach ($teams[0] as $player) {
+        $races0[] = $player['race'];
+    }
+    $races1 = [];
+    foreach ($teams[1] as $player) {
+        $races1[] = $player['race'];
+    }
+
+    if (array_diff($races0, $races1) === []) {
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+
+        array_unshift($races1, array_pop($races1));
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+
+        array_unshift($races1, array_pop($races1));
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+    }
+
+    return 1;
+}
+
+function dsr_main_next__get_status_multiplier($status) {
     if ($status === 'win') {
         return 1;
     }
