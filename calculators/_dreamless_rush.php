4756a08185d69fd879bd8227e0f586d866dbf38f
Dima Stefantsov <dima@stefantsov.com>
2023-04-22 21:58:16 +0300

    rating _dreamless_rush v1

--- calculators/_dreamless_rush.php
+++ calculators/_dreamless_rush.php
@@ -1,27 +1,42 @@
 <?php
 
-$GLOBALS['rating_calculators']['dsr_rush'] = [
+$GLOBALS['rating_calculators']['_dreamless_rush'] = [
     'description' => '
-        DSR Rush is for hotheads.
+        by <a href="/player/31532">MFDreamless</a>: rush is for hotheads v1
+        released 2023-04-22
+
         Any time you win a game under 13 minutes you gain 8 rating.
         Any time you win/lose a game over 13 minutes you lose 3 rating.
-        The maximum rank is 3000 and if you reach 3000 you are named 5508 (BOSS)
-        Rank is reset on the first date each month
+
+        The maximum rating is 3000 and if you reach 3000 you are named 5508 (BOSS).
+
+        Rating is reset to 1500 on the first date each month.
         ',
+    'logo' => [
+        'relative_url' => '/wp-content/uploads/2023/04/rating_dreamless_rush.png',
+        'width' => 500,
+        'height' => 500,
+        ],
     'default_rating' => 1500,
-    'fields' => [ // always available: game_id, player_id, team, winner_team, status
+    'fields' => [
         'g.duration',
         'g.date_played',
         ],
-    'main' => 'dsr_rush__main',
+    'main' => '_dreamless_rush__main',
     ];
 
-function dsr_rush__main(&$teams) {
+function _dreamless_rush__main(&$teams) {
+    // Reset 1st date of month.
+    if ($teams[0][0]['date_played'] < strtotime('first day of this month, midnight, UTC')) {
+        return;
+    }
+
     foreach ($teams as &$team) {
         foreach ($team as &$player) {
-            //Increases rating if game is won under 13 minutes (22.4*60*13) and decreases if not won under 13
+            // Increases rating if game is won under 13 minutes (22.4*60*13)
+            // and decreases if not won under 13.
             $diff = 0;
-            if ($player['duration'] <= 17472 && $player['winner_team'] === $player['team']) {
+            if ($player['duration'] < 17472 && $player['winner_team'] === $player['team']) {
                 $diff = 8;
             }
             else {
@@ -30,16 +45,9 @@ function dsr_rush__main(&$teams) {
 
             $player['rating'] = max(0, $player['rating'] + $diff);
 
-            // Puts anyone above 3k to 5508 (BOSS)
+            // Puts anyone above 3k to 5508 (BOSS).
             if ($player['rating'] >= 3000) {
-            $player['rating'] = 5508;
-            }
-
-            // Reset 1st date of month
-            $last_reset = date('Y-m-d', strtotime('last day of previous month'));
-            $now = $player['date_played'];
-            if ($now < $last_reset) {
-            return;
+                $player['rating'] = 5508;
             }
         }
     }
