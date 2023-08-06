3806315ef296994c867ed8bbc55f96f79c22495d
Dima Stefantsov <dima@stefantsov.com>
2023-08-06 07:31:11 +0300

    main_current v3, team/solo ratings, switch

---
+++ calculators/commanders_3v3_team.php
@@ -0,0 +1,53 @@
+<?php
+
+$GLOBALS['rating_calculators']['commanders_3v3_team'] = [
+    'description' => '
+        team commanders_3v3
+
+        same as <a href="/rating/commanders_3v3">commanders_3v3</a>, but only for team games with teammate(s) you often play with
+        games you played with random pubs do not affect this rating
+
+        now you can play some occasional solo without having to fear losing rating: your team rating is safe here!
+        ',
+    'default_rating' => $GLOBALS['rating_calculators']['commanders_3v3']['default_rating'],
+    'fields' => $GLOBALS['rating_calculators']['commanders_3v3']['fields'],
+    'main' => 'dsr_commanders_3v3_team__main',
+    ];
+
+function dsr_commanders_3v3_team__main(&$teams) {
+    $teams_with_solo_team_rating = dsr_commanders_3v3_team__get_teams_with_solo_team_rating($teams);
+
+    $GLOBALS['rating_calculators']['commanders_3v3']['main']($teams_with_solo_team_rating);
+
+    dsr_commanders_3v3_team__assign_ratings($teams, $teams_with_solo_team_rating);
+}
+
+function dsr_commanders_3v3_team__get_teams_with_solo_team_rating(&$teams) {
+    $ratings_solo_team = &$GLOBALS['rating_calculators']['commanders_3v3_team']['data']['ratings_solo_team'];
+    $teams_with_solo_team_rating = $teams;
+
+    foreach ($teams_with_solo_team_rating as &$team) {
+        foreach ($team as &$player) {
+            $player['is_solo'] = !dsr__have_teammates_in_game($player, $team);
+            $player['rating'] =
+                $ratings_solo_team[$player['is_solo']][$player['player_id']] ??
+                $GLOBALS['rating_calculators']['commanders_3v3_team']['default_rating'];
+        }
+    }
+
+    return $teams_with_solo_team_rating;
+}
+
+function dsr_commanders_3v3_team__assign_ratings(&$teams, &$teams_with_solo_team_rating) {
+    $ratings_solo_team = &$GLOBALS['rating_calculators']['commanders_3v3_team']['data']['ratings_solo_team'];
+
+    foreach ($teams_with_solo_team_rating as $team_index => $team) {
+        foreach ($team as $player_index => $player) {
+            $ratings_solo_team[$player['is_solo']][$player['player_id']] = $player['rating'];
+
+            if (!$player['is_solo']) {
+                $teams[$team_index][$player_index]['rating'] = $player['rating'];
+            }
+        }
+    }
+}
--- calculators/main_current.php
+++ calculators/main_current.php
@@ -2,68 +2,143 @@
 
 $GLOBALS['rating_calculators']['main_current'] = [
     'description' => '
-        DSR rating vCurrent
+        DSR rating v3 (current)
 
         default rating algorithm, used for most DSR rating tasks
 
-        calculates player rating according to DSR v2 rating algorithm, <a href="/changelog/rating-rules-ds-loading-screen-multiple-changes/">changelog post</a>
-        an improvement over <a href="/rating/main_v1">main_v1</a>
+        compared to <a href="/rating/main_v2">main_v2</a> it has following changes:
+        - no extra penalties for leave/afk
+        - first player to leave/afk forfeits the win for his team
+        - team rating = max player rating
+        - uncompressing ratings before comparisons
+        - fixed 2k-border gravity bug
+        - compression now starts below 1000 rating
+        - improved rewards calculations
+        - fixed team1 and team2 rewards rounded differently
+        - teams mirror matchup gives x2 rating changes
         ',
     'default_rating' => 2000,
-    'fields' => [],
+    'fields' => ['gp.race', 'gp.status_timing'],
     'main' => 'dsr_main_current__main',
     ];
 
 function dsr_main_current__main(&$teams) {
-    $players_max = max(count($teams[0]), count($teams[1]));
-    $q = $players_max**2;
+    dsr_main_current__uncompress_rating($teams);
+    dsr_main_current__update_ratings($teams);
+    dsr_main_current__compress_rating($teams);
+}
 
-    $team_rating = [];
-    $team_rating[0] = round((from($teams[0])->aggregate(fn($a, $v) => $a + $v['rating']**$q, 0) / count($teams[0]))**(1/$q));
-    $team_rating[1] = round((from($teams[1])->aggregate(fn($a, $v) => $a + $v['rating']**$q, 0) / count($teams[1]))**(1/$q));
-    $factor = 4000;
-    if ($team_rating[0] < 2000 && $team_rating[1] < 2000) {
-        $factor = 400;
-    }
-
-    $reward = [];
-    $reward[0] = max(floor(100 / (1 + 10**(($team_rating[0] - $team_rating[1])/$factor))), 1);
-    $reward[1] = 100 - $reward[0];
-
+function dsr_main_current__uncompress_rating(&$teams) {
     foreach ($teams as &$team) {
         foreach ($team as &$player) {
-            $multiplier = dsr_main_current__get_multiplier($player['status']);
-            $diff = intval($multiplier*$reward[$player['winner_team']]);
-            if ($player['rating'] < 2000) {
-                $diff = $diff / 10;
-                if ($diff < 0) {
-                    $diff = min(-1, intval(ceil($diff)));
-                }
-                else {
-                    $diff = max(1, intval(floor($diff)));
-                }
+            if ($player['rating'] > 1000) {
+                $player['rating'] = $player['rating'] + 9000;
             }
-            $player['rating'] = max($player['rating'] + $diff, 1);
+            else {
+                $player['rating'] = intval($player['rating']*10);
+            }
         }
     }
 }
 
-function dsr_main_current__get_multiplier($status) {
-    if ($status === 'lose') {
-        return -1;
+function dsr_main_current__compress_rating(&$teams) {
+    foreach ($teams as &$team) {
+        foreach ($team as &$player) {
+            if ($player['rating'] > 10000) {
+                $player['rating'] = $player['rating'] - 9000;
+            }
+            else {
+                $player['rating'] = intval(round($player['rating']/10));
+            }
+        }
+    }
+}
+
+function dsr_main_current__update_ratings(&$teams) {
+    $full_mirror_multiplier = dsr_main_current__get_full_mirror_multiplier($teams);
+
+    $team_ratings = [];
+    $team_ratings[0] = from($teams[0])->max('$v["rating"]');
+    $team_ratings[1] = from($teams[1])->max('$v["rating"]');
+
+    $team_forfeit =
+        from($teams)->
+        selectmany()->
+        where('$v["status_timing"] != -1')->
+        orderby('$v["status_timing"]')->
+        select('$v["team"]')->
+        firstordefault(false);
+
+    $rewards = [];
+    $rewards[0] = round(100 / (1 + 10**(($team_ratings[0] - $team_ratings[1])/4000)));
+    $rewards[1] = 100 - $rewards[0];
+    $rewards[2] = $rewards[0];
+
+    foreach ($teams as &$team) {
+        foreach ($team as &$player) {
+            $status_multiplier = dsr_main_current__get_status_multiplier($player['status']);
+
+            $reward = $rewards[$player['winner_team']];
+            if ($player['team'] === $player['winner_team'] && $player['status'] !== 'win') {
+                $reward = $rewards[$player['winner_team']+1];
+            }
+
+            $diff = $reward*$full_mirror_multiplier*$status_multiplier;
+            if ($player['rating'] + $diff < 10000) {
+                $diff = max(1, floor($reward*$full_mirror_multiplier/10))*10 * $status_multiplier;
+            }
+
+            // once a game was forfeited,
+            // members of forfeited team can't get more than 0, but can lose full
+            // members of remaining team can't lose more than 0, but can win full
+            if ($team_forfeit !== false) {
+                if ($player['team'] === $team_forfeit) {
+                    $diff = min(0, $diff);
+                }
+                else {
+                    $diff = max(0, $diff);
+                }
+            }
+
+
+            $player['rating'] = max(10, $player['rating'] + intval($diff));
+        }
+    }
+}
+
+function dsr_main_current__get_full_mirror_multiplier(&$teams) {
+    $races0 = [];
+    foreach ($teams[0] as $player) {
+        $races0[] = $player['race'];
+    }
+    $races1 = [];
+    foreach ($teams[1] as $player) {
+        $races1[] = $player['race'];
     }
 
+    if (array_diff($races0, $races1) === []) {
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+
+        array_unshift($races1, array_pop($races1));
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+
+        array_unshift($races1, array_pop($races1));
+        if ($races0[0] === $races1[0] && ($races0[1]??false) === ($races1[1]??false) && ($races0[2]??false) === ($races1[2]??false)) {
+            return 2;
+        }
+    }
+
+    return 1;
+}
+
+function dsr_main_current__get_status_multiplier($status) {
     if ($status === 'win') {
         return 1;
     }
 
-    if ($status === 'leaver') {
-        return -3;
-    }
-
-    if ($status === 'afk') {
-        return -5;
-    }
-
-    return 0;
+    return -1;
 }
